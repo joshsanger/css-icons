3013de98f551b54d180cc0178329c05429347cfb
Josh S <josh.sanger@geekpower.ca>
2017-08-08 12:08:07 -0400

    Added in person icon.

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php require_once('./_includes/config.php');?>
 <?php require_once('./_includes/header.php');?>
 
+<div class="container">
+    <i class="css-icons person"></i>
+    <i class="css-icons person line"></i>
+</div> <!-- /container -->
+
 <?php require_once('./_includes/footer.php');?>
