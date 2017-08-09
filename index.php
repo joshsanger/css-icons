cbb97b8864cb69d47424d621994a756aadfe5216
Josh S <josh.sanger@geekpower.ca>
2017-08-09 14:24:22 -0400

    Added in more icons.

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <div class="container">
     <i class="css-icons person"></i>
     <i class="css-icons person line"></i>
+    <i class="css-icons clock"></i>
+    <i class="css-icons download"></i>
+    <i class="css-icons upload"></i>
 </div> <!-- /container -->
 
 <?php require_once('./_includes/footer.php');?>
