b493fa7dfaef2f7376eefea97ba2c4152140dd10
Josh S <josh.sanger@geekpower.ca>
2017-08-09 16:31:02 -0400

    Added in more icons.

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <i class="css-icons clock"></i>
     <i class="css-icons download"></i>
     <i class="css-icons upload"></i>
+    <i class="css-icons search"></i>
+    <i class="css-icons bars"></i>
+    <i class="css-icons dots"></i>
+    <i class="css-icons dots vertical"></i>
+    <i class="css-icons chevron left"></i>
+    <i class="css-icons chevron right"></i>
+    <i class="css-icons chevron up"></i>
+    <i class="css-icons chevron down"></i>
+    <i class="css-icons chat"></i>
+    <i class="css-icons chat line"></i>
 </div> <!-- /container -->
 
 <?php require_once('./_includes/footer.php');?>
