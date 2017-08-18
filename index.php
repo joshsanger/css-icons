3ee0d9d690bed6f92093a405027528365a67c082
Josh S <josh.sanger@geekpower.ca>
2017-08-18 12:34:05 -0400

    Removed the star icon and started working on the demo.

--- index.php
+++ index.php
@@ -2,48 +2,172 @@
 <?php require_once('./_includes/header.php');?>
 
 <div class="container">
-    <i class="css-icons star"></i>
-    <i class="css-icons person"></i>
-    <i class="css-icons person line"></i>
-    <i class="css-icons clock"></i>
-    <i class="css-icons download"></i>
-    <i class="css-icons upload"></i>
-    <i class="css-icons search"></i>
-    <i class="css-icons bars"></i>
-    <i class="css-icons dots"></i>
-    <i class="css-icons dots vertical"></i>
-    <i class="css-icons chevron left"></i>
-    <i class="css-icons chevron right"></i>
-    <i class="css-icons chevron up"></i>
-    <i class="css-icons chevron down"></i>
-    <i class="css-icons chat"></i>
-    <i class="css-icons chat line"></i>
-    <i class="css-icons plus"></i>
-    <i class="css-icons minus"></i>
-    <i class="css-icons close"></i>
-    <i class="css-icons check"></i>
-    <i class="css-icons face happy"></i>
-    <i class="css-icons face sad"></i>
-    <i class="css-icons credit-card"></i>
-    <i class="css-icons calendar"></i>
-    <i class="css-icons heart"></i>
-    <i class="css-icons not-allowed"></i>
-    <i class="css-icons home"></i>
-    <i class="css-icons info"></i>
-    <i class="css-icons label"></i>
-    <i class="css-icons map-marker"></i>
-    <i class="css-icons arrow left"></i>
-    <i class="css-icons arrow right"></i>
-    <i class="css-icons arrow up"></i>
-    <i class="css-icons arrow down"></i>
-    <i class="css-icons arrow left alt"></i>
-    <i class="css-icons arrow right alt"></i>
-    <i class="css-icons arrow up alt"></i>
-    <i class="css-icons arrow down alt"></i>
-    <i class="css-icons mail"></i>
-    <i class="css-icons edit"></i>
-    <i class="css-icons undo"></i>
-    <i class="css-icons trash"></i>
+    <div class="flex-wrapper" id="icon-wrap">
+        <span class="demo-item">
+            <i class="css-icons person"></i>
+                <span>person</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons person line"></i>
+                <span>person line</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons clock"></i>
+                <span>clock</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons download"></i>
+                <span>download</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons upload"></i>
+                <span>upload</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons search"></i>
+                <span>search</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons bars"></i>
+                <span>bars</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons dots"></i>
+                <span>dots</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons dots vertical"></i>
+                <span>dots vertical</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons chevron left"></i>
+                <span>chevron left</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons chevron right"></i>
+                <span>chevron right</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons chevron up"></i>
+                <span>chevron up</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons chevron down"></i>
+                <span>chevron down</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons chat"></i>
+                <span>chat</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons chat line"></i>
+                <span>chat line</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons plus"></i>
+                <span>plus</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons minus"></i>
+                <span>minus</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons close"></i>
+                <span>close</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons check"></i>
+                <span>check</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons face happy"></i>
+                <span>face happy</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons face sad"></i>
+                <span>face sad</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons credit-card"></i>
+                <span>credit-card</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons calendar"></i>
+                <span>calendar</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons heart"></i>
+                <span>heart</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons not-allowed"></i>
+                <span>not-allowed</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons home"></i>
+                <span>home</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons info"></i>
+                <span>info</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons label"></i>
+                <span>label</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons map-marker"></i>
+                <span>map-marker</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow left"></i>
+                <span>arrow left</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow right"></i>
+                <span>arrow right</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow up"></i>
+                <span>arrow up</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow down"></i>
+                <span>arrow down</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow left alt"></i>
+                <span>arrow left alt</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow right alt"></i>
+                <span>arrow right alt</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow up alt"></i>
+                <span>arrow up alt</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons arrow down alt"></i>
+                <span>arrow down alt</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons mail"></i>
+                <span>mail</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons edit"></i>
+                <span>edit</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons undo"></i>
+                <span>undo</span>
+        </span>
+            <span class="demo-item">
+            <i class="css-icons trash"></i>
+                <span>trash</span>
+        </span>
+    </div> <!-- /flex-wrapper -->
 </div> <!-- /container -->
 
 <?php require_once('./_includes/footer.php');?>
